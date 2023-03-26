bcc14508f2dfedd037bf67501bddbc95ee64a495
Tugran Yayla <tugrandev@gmail.com>
2023-03-26 22:05:55 +0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Tuğran on 26.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
